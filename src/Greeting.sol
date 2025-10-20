f2266be6c5f7c9318331ef20b1889303447f1c35
Winnie Malusha <malushlywinnie@gmail.com>
2025-10-21 00:04:51 +0300

    pre-commit: apply forge fmt to staged contracts

--- src/Greeting.sol
+++ src/Greeting.sol
@@ -1,23 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Greeter{
+contract Greeter {
     string public greeting;
 
-
-    constructor(string memory _initialGreeting){
+    constructor(string memory _initialGreeting) {
         greeting = _initialGreeting;
     }
-    
-    function setGreeting(string memory _newGreeting) public{
+
+    function setGreeting(string memory _newGreeting) public {
         greeting = _newGreeting;
     }
 
-    function greet() public view returns(string memory){
+    function greet() public view returns (string memory) {
         return greeting;
-    }    
-
-    
-
+    }
 }
 
